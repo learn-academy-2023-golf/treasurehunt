7449535ed3677424b68a9e46e71e7efb47acef35
Sarah Proctor <sproctor950@gmail.com>
2022-08-29 19:14:44 -0700

    :white_check_mark: adds react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it("renders header", () => {
+  render(<App />)
+  const header = screen.getByText("Treasure Hunt Game")
+})
